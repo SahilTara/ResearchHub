ca0c43762e13f36d9002d4aefdb390d1c63052a6
dandua98 <danish@dandua.com>
2019-02-09 16:36:57 -0800

    removed ios as submodule

---
+++ src/iOS/ResearchHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResearchHub
+//
+//  Created by Danish Dua on 2019-02-09.
+//  Copyright © 2019 Danish Dua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
