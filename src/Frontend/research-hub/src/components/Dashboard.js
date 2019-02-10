3e02778b900a3e76eb6447a56744c544842c82b8
Kai-Bailey <kbailey1@ualberta.ca>
2019-02-10 04:08:10 -0800

    Finished login, set up dashboard and new study page

--- src/Frontend/research-hub/src/components/Dashboard.js
+++ src/Frontend/research-hub/src/components/Dashboard.js
@@ -1,9 +1,139 @@
-import React from "react";
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
+import axios from "axios";
+import PortalLogoBlue from "../images/PortalLogoBlue.svg";
+import "./Dashboard.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+const data = [
+  { name: "Feb-1", pv: 2400 },
+  { name: "Feb-2", pv: 1398 },
+  { name: "Feb-3", pv: 9800 },
+  { name: "Feb-4", pv: 3908 },
+  { name: "Feb-5", pv: 4800 },
+  { name: "Feb-6", pv: 3800 },
+  { name: "Feb-6", pv: 4300 }
+];
 
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>This is the Dashboard</h1>
-    </div>
-  );
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // fetch("https://research-hub-cs.azurewebsites.net/api/researchPosting/all", {
+  //   mode: "cors"
+  // })
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(myJson) {
+  //     console.log(JSON.stringify(myJson));
+  //   });
+
+  componentDidMount() {
+    console.log("Mounted!!!!!!!!!");
+    axios
+      .get("https://research-hub-cs.azurewebsites.net/api/researchPosting/all")
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>Does sleep deprivation impact problematic eating?</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 1 }}>
+            <Nav defaultActiveKey="/home" className="flex-column">
+              <Nav.Item className="mr-5" as="li">
+                <Image src={PortalLogoBlue} width="20" />
+                <Nav.Link href="/home">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <Nav defaultActiveKey="/home" as="ul">
+                  <Nav.Item as="li">
+                    <Row>
+                      <Col md={{ span: 1 }}>
+                        <Image src={PortalLogoBlue} width="20" />
+                      </Col>
+                      <Col>
+                        <Nav.Link href="/home">Active</Nav.Link>
+                      </Col>
+                    </Row>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Row>
+                      <Col md={{ span: 1 }}>
+                        <Image src={PortalLogoBlue} width="20" />
+                      </Col>
+                      <Col>
+                        <Nav.Link href="/home">Active</Nav.Link>
+                      </Col>
+                    </Row>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Row>
+                      <Col md={{ span: 1 }}>
+                        <Image src={PortalLogoBlue} width="20" />
+                      </Col>
+                      <Col>
+                        <Nav.Link href="/home">Active</Nav.Link>
+                      </Col>
+                    </Row>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <LineChart
+                  width={600}
+                  height={300}
+                  data={data}
+                  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                >
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Line
+                    type="monotone"
+                    dataKey="pv"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                  />
+                </LineChart>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
--- src/Frontend/research-hub/src/components/NewStudy.js
+++ src/Frontend/research-hub/src/components/NewStudy.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-export default function NewStudy() {
-  return <div>New study</div>;
+export default class NewStudy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onNewPost = this.onNewPost.bind(this);
+  }
+
+  onNewPost(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    this.setState({ [name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>New Study</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form onSubmit={this.onNewPost}>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Title</Form.Label>
+                <Form.Control placeholder="Title..." />
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  name="description"
+                  onChange={this.handleChange}
+                  as="textarea"
+                  rows="3"
+                />
+              </Form.Group>
+              <Button variant="outline-secondary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
